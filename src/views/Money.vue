93610ebfa0068ec27a0b70d0c8889dd1032aa3ca
SranHuang_1005 <763913797@qq.com>
2021-05-20 17:38:06 +0800

    init

---
+++ src/views/Money.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Money</div>
+  <!-- <Nav /> -->
+</template>
+
+<script>
+export default {
+  name: "Money",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>当前路径不存在</div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
